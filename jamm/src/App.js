59f6c093d163931854b86d28079c059ccb9841af
Allison Hahn <allisonk.hahn@gmail.com>
2018-07-16 14:01:00 -0700

    testing card

--- jamm/src/App.js
+++ jamm/src/App.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          <code>Wazzuppp</code> 
-        </p>
+      <div className="card">
+      <div className="img-container">
+        <img alt="dog" src="./public/images/dog.png" />
       </div>
+      <div className="content">
+        <ul>
+          <li>
+            <strong>Type of Pet:</strong> Dog
+          </li>
+          <li>
+            <strong>Description:</strong> Good Dog
+          </li>
+          <button type="button" class="btn btn-dark" value="400">Buy Pet for 400 coins</button>
+        </ul>
+      </div>
+    </div>
     );
   }
 }
