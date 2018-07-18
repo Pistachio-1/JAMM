e1bda690022f83be7094b0d23cea77351e322eec
Mike West <mike.wesley.west@gmail.com>
2018-07-17 21:57:17 -0700

    added mocha/chai tests for Learner model
    lots of new packages added

--- jamm/models/learner.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(sequelize, DataTypes) { 
-    var Learner = database.define("learner", {
-        firstName : Sequelize.STRING,
-        lastName  : Sequelize.STRING,
-        age       : Sequelize.INTEGER,
-        favoritePet: Sequelize.STRING,
-        createdAt : Sequelize.DATE,
-        updatedAt : Sequelize.DATE
-});
