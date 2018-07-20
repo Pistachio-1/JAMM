ea5fcbb109ae88773dd080679eb50bca881eff16
Mike West <mike.wesley.west@gmail.com>
2018-07-20 09:51:41 -0700

    delete obsolete file

--- jamm/src/models/learner.js
+++ jamm/src/models/learner.js
@@ -8,13 +8,14 @@ const sequelize = database.sequelize;
 // * the primary key (id) is implied (auto-increment)
 const Learner = (sequelize, DataTypes) => {
     var Learner = sequelize.define("learner", {
-        firstName: Sequelize.STRING,
-        lastName: Sequelize.STRING,
-        email: Sequelize.STRING,
-        coins: Sequelize.INTEGER,
-        favoritePet: Sequelize.STRING,
-        createdAt: Sequelize.DATE,
-        updatedAt: Sequelize.DATE
+        firstName: {type: Sequelize.STRING, allowNull: false},
+        lastName:  {type: Sequelize.STRING, allowNull: false},
+        userName:  {type: Sequelize.STRING, allowNull: false},
+        email:     {type: Sequelize.STRING, allowNull: false},
+        coins:     {type: Sequelize.INTEGER, defaultValue: 20},
+        favoritePet: {type: Sequelize.STRING, allowNull: false},
+        createdAt: {type: Sequelize.DATE, defaultValue: Sequelize.NOW},
+        updatedAt: {type: Sequelize.DATE, defaultValue: Sequelize.NOW}
     });
 
     return Learner;
