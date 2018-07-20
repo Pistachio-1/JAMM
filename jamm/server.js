5c4aa275b639f9e6c9fc0aeabe9ac47dadbef2a1
Mike West <mike.wesley.west@gmail.com>
2018-07-19 19:35:25 -0700

    added body-parser to express app

--- jamm/server.js
+++ jamm/server.js
@@ -1,6 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 3001;
+
+// * parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// * parse application/json
+app.use(bodyParser.json());
 
 require("./routes/learner-routes")(app);
 
